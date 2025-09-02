2feff286c840963aecf5e8257af83519a145b442
Nicolò Rinaldi <nicolo.rinaldi99@gmail.com>
2025-09-02 10:24:54 +0200

    [DAGE-55] First stage of the refactor Right now the RreWriter doesn't work, due to the use of the `config.py` file in the dataset_generator package. `pyproject.toml` files have been built for each of the subfolders, with only the needed dependencies. General mypy (in `pyproject.toml`) and ruff configuration settings are in `rre-tools/` folder.
    Still need to add: resource folder in `rre-tools/`, change RreWriter, change the datastore to save in resource folder.
    
    New structure is the following:
    
    rre-tools
    ├──commons (contains all the common classes/functions)
    ├──dataset-generator (contains just the classes for the dataset generator project)
    ├──docker-services (contains all the container for the different search engines)
    └──src (will contain the script to run the full pipeline)

--- rre-dataset-generator/src/llm/__init__.py
+++
@@ -1,9 +0,0 @@
-from .llm_config import LLMConfig
-from .llm_provider_factory import LLMServiceFactory
-from .llm_service import LLMService
-
-__all__ = [
-    "LLMConfig",
-    "LLMServiceFactory",
-    "LLMService",
-]
